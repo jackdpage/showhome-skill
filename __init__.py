18b2aa3121ca9e5fe914aaaf916c1e2257123987
Jack Page <jdpboc98@gmail.com>
2019-01-08 22:55:33 +0000

    Add support for X32 + Interact with X32 API in showhome + Separate intent files

--- __init__.py
+++ __init__.py
@@ -5,17 +5,43 @@ import requests
 class Showhome(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_file_handler('showhome.intent')
-    def handle_showhome(self, message):
-        loc = message.data['loc']
-        state = message.data['state']
+        
         ip = self.settings['ip']
         port = self.settings['port']
+        self.base_url = 'http://'+ip+':'+str(port)
 
-        url = 'http://'+ip+':'+str(port)+'/eos/group/apply_preset/label'
+    @intent_file_handler('eos.apply.intent')
+    def handle_eos(self, message):
+        loc = message.data['loc']
+        state = message.data['state']
+
+        url = self.base_url+'/eos/group/apply_preset/label'
 
         request = requests.put(url, data={'loc': loc, 'state': state})
+
+        if request.text != 'null':
+            self.speak_dialog('showhome.error', {'text': request.text})
+
+    @intent_file_handler('x32.send.intent')
+    def handle_x32_on(self, message):
+        src = message.data['src']
+        dest = message.data['dest']
+
+        url = self.base_url+'/x32/ch/on/label'
+
+        request = requests.put(url, data={'src': src, 'dest': dest})
+
+        if request.text != 'null':
+            self.speak_dialog('showhome.error', {'text': request.text})
+
+    @intent_file_handler('x32.stop.intent')
+    def handle_x32_off(self, message):
+        src = message.data['src']
+        dest = message.data['dest']
+
+        url = self.base_url+'/x32/ch/off/label'
+
+        request = requests.put(url, data={'src': src, 'dest': dest})
 
         if request.text != 'null':
             self.speak_dialog('showhome.error', {'text': request.text})
